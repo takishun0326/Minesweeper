fa6e9c9785dbdcdfcf32b7520c1914aa7b18f00b
takishun0326 <takishun.st3260@gmail.com>
2021-12-22 22:21:51 +0900

    mapping

--- Frame/Frame.py
+++ Frame/Frame.py
@@ -1,7 +1,7 @@
 import tkinter
 from tkinter import ttk
 
-#from ..Game import Game
+from Game import Game
 
 WIDTH = 500
 HEIGHT = 500
@@ -9,6 +9,7 @@ HEIGHT = 500
 class Frame:
     def __init__(self, main_win):
         self.main_win = main_win
+        self.game = Game.Game()
 
         self.frame = tkinter.Frame(main_win, width=500, height=500)
         self.frame.pack()
@@ -19,14 +20,17 @@ class Frame:
 
 
     def pushed_grid(self,i,j):
-        def tmp():
+        def disable_button():
+            self.game.gameStart(i,j)
+            # 押されたマスを押された状態で保持された物へ変更
             self.buttons[i][j].destroy()
             self.buttons[i][j] = tkinter.Button(self.main_win,text='0', relief='sunken')
             self.buttons[i][j].place(x=20*i+100, y=20*j+100, width=20, height=20)
             print("pushed " + str(i) + str(j))
-        return tmp
 
+        return disable_button
 
+    # widgets 準備
     def widgets(self):
         self.buttons = []
         for i in range(15):
@@ -36,6 +40,7 @@ class Frame:
             
             self.buttons.append(button)
 
+    # widgets 配置
     def position(self):
         for i in range(15):
             for j in range(15):
--- Game/Game.py
+++ Game/Game.py
@@ -2,19 +2,44 @@ import random
 
 class Game:
     def __init__(self):
-        
-        # Mine n%
-        per = 20
+        self.is_gamestart = False
 
-        self.mines = []
+    def gameStart(self, clicked_x, clicked_y):
+
+        self.is_gamestart = True
+        # Mine n%
+        per = 15
+
+        # map作成 15x15のすべて値が0
+        self.map = [[0 for i in range(15)] for j in range(15)]
         for i in range(15):
             for j in range(15):
-                rand = random.randint(0,100)
-                if rand <= per:
-                    # per % の確率でヒットした座標を記録
-                    self.mines.append(list((i,j)))
+                # クリックした場所なら
+                if not (i==clicked_x and j==clicked_y):
+                    rand = random.randint(0,100)
+                    if rand <= per:
+                        # per % の確率でヒットした座標を地雷にする(-1)
+                        self.map[i][j] = -1
+                        
+        print (self.map)
+        dx = [1, 1, 1, 0, 0, -1, -1, -1]
+        dy = [1, 0, -1, 1, -1, 1, 0, -1]
+
+        for i in range(15):
+            for j in range(15):
+
+                if self.map[i][j] != -1:
+                    # 画面外へはアクセスしない
+                    for k in range(8):
+                        if (i+dx[k]>=0) and (i+dx[k]<15) and (j+dy[k]>=0) and (j+dy[k]<15) :
+                            if self.map[i+dx[k]][j+dy[k]] == -1:
+                                # 地雷の数をカウント
+                                self.map[i][j] += 1
+                    
         
 
+        
 
+    
 
 
