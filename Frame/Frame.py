c13ebfb2b953faedd8a4d81ce7cec167bb140757
takishun0326 <takishun.st3260@gmail.com>
2021-12-23 13:59:06 +0900

    旗を複数置けるようになたよぉ

--- Frame/Frame.py
+++ Frame/Frame.py
@@ -1,3 +1,4 @@
+from os import kill
 import tkinter
 from tkinter import ttk
 
@@ -12,6 +13,8 @@ class Frame:
         self.main_win = main_win
         self.game = Game.Game()
 
+        self.flgs = []
+
         self.frame = tkinter.Frame(main_win, width=500, height=500)
         self.frame.pack()
 
@@ -21,18 +24,26 @@ class Frame:
     # 押された後のボタンの状態  x, y, text
     def disable_button(self, i, j, num):
             
-            # 押されたマスを押された状態で保持された物へ変更
-            self.buttons[i][j].destroy()
+        # 押されたマスを押された状態で保持された物へ変更
+        self.buttons[i][j].destroy()
+        if num == 0:
+            self.buttons[i][j] = tkinter.Button(self.main_win, relief='sunken')
+        else :    
             self.buttons[i][j] = tkinter.Button(self.main_win, text=num , relief='sunken')
-            self.buttons[i][j].place(x=20*i+100, y=20*j+100, width=20, height=20)
-            # print("pushed " + str(i) + str(j))
+        self.buttons[i][j].place(x=20*i+100, y=20*j+100, width=20, height=20)
+        # print("pushed " + str(i) + str(j))
 
-    def put_flg(self,event, i, j):
+    # 右クリックで旗を置く
+    def put_flg(self,event):
         # gameがスタートしているか
         if self.game.is_gamestart:
             flg_img = tkinter.PhotoImage(file = 'Flag.png')
-            self.small_flg_img = flg_img.subsample(30, 30)
-            event.widget["image"] = self.small_flg_img
+            self.flgs.append(flg_img.subsample(30, 30))
+            event.widget["image"] = self.flgs[-1]
+
+    # 座標からどのインデックスか教える
+    def get_index(place):
+        return (place-100)/20
 
     def pushed_grid(self,i,j):
         def expanding_pushed_button():
@@ -67,8 +78,9 @@ class Frame:
         for i in range(15):
             button = []
             for j in range(15):
+                # ボタンの設定
                 b = tkinter.Button(self.main_win, command=self.pushed_grid(i,j))
-                b.bind("<Button-3>", lambda event: self.put_flg(event,i,j))                 
+                b.bind("<Button-3>", lambda event: self.put_flg(event))                 
                 button.append(b)
             
             self.buttons.append(button)
