c053345d29ce8b864c04902bfef14e57cab6c894
takishun0326 <takishun.st3260@gmail.com>
2021-12-23 19:57:04 +0900

    add RestartButton with Smile

--- Frame/FrameMaster.py
+++ Frame/FrameMaster.py
@@ -8,7 +8,10 @@ class FrameMaster:
         self.main_win = main_win
         self.frame = Frame.Frame(self.main_win)
         
-        self.restart_button = tkinter.Button(self.main_win, text='restart', command=self.game_restart)
-        self.restart_button.place(x=10, y=20, width=60, height=60)
+
+        smile_img = tkinter.PhotoImage(file = 'Smile.png')
+        self.s_smile_img = smile_img.subsample(10,10)
+        self.restart_button = tkinter.Button(self.main_win, image=self.s_smile_img, command=self.game_restart)
+        self.restart_button.place(x=170, y=20, width=60, height=60)
     def game_restart(self):
         self.frame = Frame.Frame(self.main_win)
